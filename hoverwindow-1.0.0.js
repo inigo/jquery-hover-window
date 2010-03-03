479e0e80fe037a8e97d9d0bcd3114b8d324c1899
Inigo <inigo.surguy@gmail.com>
2010-03-03 07:48:09 +0000

    Don't fade out when over the preview window

--- hoverwindow-1.0.0.js
+++ hoverwindow-1.0.0.js
@@ -1,7 +1,7 @@
 /**
 	hoverWindow displays a pop-up window when you hover over a content element.
 	
-	Typical usage:
+	Typical usage (which will use the href attribute to display an iframe)
 
 	$(document).ready(function() {
 		$('a').hoverWindow();
@@ -31,40 +31,65 @@
 		log("Setting up hoverWindow");
 
 		var selectedItem;
+		var cancellingPreview;
 
 		var doPreview = function(itemToPreview) {
 		    if (itemToPreview!=selectedItem || !itemToPreview) {
 			log("Selected item has changed - cancelling preview");
 			return;
 		    }
-		    if ($("#hoverWindow_searchPreviewPane").length == 0) {
-		      $("body").append("<div id='hoverWindow_searchPreviewPane' style='position: absolute'><iframe src=''></iframe></div>");
-		    }
-		    var href = options.src(itemToPreview);    
+		    var previewPane = $("#hoverWindow_searchPreviewPane");
+		    var href = options.src(itemToPreview); 
 		    var coords = new Object();
 		    coords.top = Math.floor(selectedItem.offset().top)+10+selectedItem.height();
 		    coords.left = Math.floor(selectedItem.offset().left)+50;
 		    log("Showing preview pane at "+coords.top+","+coords.left+" for "+href);
-		    $("#hoverWindow_searchPreviewPane iframe").attr("src", href);    
-		    $("#hoverWindow_searchPreviewPane").offset(coords).fadeIn("fast");
+		    // $("#hoverWindow_searchPreviewPane iframe").attr("src", href);    
+		    previewPane.offset(coords).fadeIn("fast");
 		};
 
 		var prepareForPreview = function() {
 		    log("Preparing for preview - will do preview shortly");
+		    cancellingPreview = null;
 		    if (selectedItem!=$(this)) {
 			selectedItem = $(this);
 			var href = options.src(selectedItem);
-			$("#hoverWindow_searchPreviewPane iframe").attr("src", href);
+		        var previewPane = $("#hoverWindow_searchPreviewPane");
+			if (previewPane.length == 0) {
+			  $("body").append("<div id='hoverWindow_searchPreviewPane' style='position: absolute'><iframe src=''></iframe></div>");
+			  previewPane = $("#hoverWindow_searchPreviewPane");
+			  previewPane.hover(function() { cancellingPreview = null; }, prepareToCancel);
+			}
+			var previewFrame = $("#hoverWindow_searchPreviewPane iframe");
+			if (previewFrame.attr("src")!=href) {
+				previewFrame.attr("src", href);
+		    		previewPane.filter(":visible").fadeOut("fast");
+				log("Preview href has changed - now "+href+" but was "+previewFrame.attr("src"));
+			} else {
+				log("Preview href has not changed - now "+href+" but was "+previewFrame.attr("src"));
+			}
 			setTimeout(function() { doPreview(selectedItem); }, 1000);
 		    }
 		};
 
 		var cancelPreview = function() {
-		    log("Cancelling preview");
-		    selectedItem = null;
-		    $("#hoverWindow_searchPreviewPane").fadeOut("fast");
+		    if (cancellingPreview) {
+			log("Cancelling preview");
+			selectedItem = null;
+			cancellingPreview = null;
+			$("#hoverWindow_searchPreviewPane").fadeOut("fast");
+		    } else {
+			log("Not cancelling preview - called, but no longer cancelling");
+		    }
 		};
 
-		return this.hover(prepareForPreview, cancelPreview);
+		var prepareToCancel = function() {
+		    if (!cancellingPreview) {
+			cancellingPreview = true;
+		    	setTimeout(cancelPreview, 1000);
+		    }
+		}
+
+		return this.hover(prepareForPreview, prepareToCancel);
 	};  
 })(jQuery); 
